9499fcd571eb0afe4565a485ded5226431e8e70d
Maël Pignet <mpignet20@gmail.com>
2024-02-22 10:06:23 +0100

    ugly but passing the tests

--- RPN.rb
+++ RPN.rb
@@ -1,14 +1,15 @@
 
 class RPN
 
-  def initialize(input)
-    @input = input
-  end
-
-  def calculator
-    expression = @input.split(" ")
+  def calculate(input)
+    expression = input.split(" ")
     stack = []
     expression.each do |element|
+      # puts "Element: #{element}"
+      if element == "+" || element == "-" || element == "*" || element == "/"
+        num2 = stack.pop
+        num1 = stack.pop
+      end
       case element
         when "+"
           stack.push(num1 + num2)
@@ -21,8 +22,10 @@ class RPN
         else
           stack.push(element.to_i)
       end
+      # puts "Stack: #{stack}"
     end
+    return stack.pop
   end
 
-end
 
+end
--- test.rb
+++ test.rb
@@ -1,19 +1,25 @@
 require_relative './rpn.rb'
 require 'rspec/autorun'
 
-class TestRPN
+class TestRPM
 
-  def test_rpn
-    assert_equal 3, RPN.calculate("1 2 +")
-    # assert_equal 2, RPN.calculate("4 2 /")
-    # assert_equal 5, RPN.calculate("3 2 + 4 -")
-    # assert_equal 3, RPN.calculate("3 2 + 4 - 2 +")
-    # assert_equal 5, RPN.calculate("3 2 + 4 - 2 + 3 *")
-    # assert_equal 15, RPN.calculate("3 2 + 4 - 2 + 3 * 5 +")
-    # assert_equal 15, RPN.calculate("3 2 + 4 - 2 + 3 * 5 + 1 -")
+  describe RPN do
+    rpn = RPN.new
+    it "should return 3" do
+      expect(rpn.calculate("1 2 +")).to eq(3)
+    end
+    it "should return 6" do
+      expect(rpn.calculate("1 2 + 2 *")).to eq(6)
+    end
+    it "should return 5" do
+      expect(rpn.calculate("1 2 + 2 * 1 -")).to eq(5)
+    end
+    it "should return 14" do
+      expect(rpn.calculate("5 1 2 + 4 * 3 - +")).to eq(14)
+    end
+    it "should return 2" do
+      expect(rpn.calculate("4 2 5 * + 1 3 2 * + /")).to eq(2)
+    end
   end
 
-  it "should return 3" do
-    expect(RPN.calculate("1 2 +")).to eq(3)
-  end
 end
